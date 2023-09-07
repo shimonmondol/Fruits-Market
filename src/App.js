3298a53a3fe81e14ff30be1c91dfe47523f6cc74
Shimon Mondol <Shimonuap072@gmail.com>
2023-09-08 02:38:42 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -18,15 +18,6 @@ const App = () => {
                     <Cart />
                 </div>
             </div>
-
-            <footer>
-                <h1>How React Works?</h1>
-                <h3>React is a JavaScript library that creates user interfaces in a predictable and efficient way using declarative code. I can use it to help build single page applications and mobile apps.</h3>
-
-                <h1>How useState Works?</h1>
-                <h3>useState is a Hook that allows to state variables in functional components. I can pass the initial state to this function and it returns a variable with the current state value and another function to update this value.</h3>
-
-            </footer>
         </div>
     );
 }
